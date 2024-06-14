664caf5f98bcd9bc661ef68050dcd3ac589d833a
Louisfhgr <94702147+Louisfhgr@users.noreply.github.com>
2024-06-14 19:19:47 +0200

    Initial commit

---
+++ plugins/supabase.js
@@ -0,0 +1,9 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://cohjwqndltomjwltqqjw.supabase.co';  
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNvaGp3cW5kbHRvbWp3bHRxcWp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgzODQwOTIsImV4cCI6MjAzMzk2MDA5Mn0.z3Q_Vr27Vm_NVxgBvLeGNGSjO8_3U5ZZ_mua0OeWld0'; 
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default defineNuxtPlugin(nuxtApp => {
+  nuxtApp.provide('supabase', supabase);
+});
